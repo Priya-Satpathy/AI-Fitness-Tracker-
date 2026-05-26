d419b4cc5b00fd67451f6f35309d65f9f7c7930e
Priya Satpathy <priyasatpathy429@gmail.com>
2026-05-26 19:03:11 +0530

    content added

--- client/src/pages/Layout.tsx
+++ client/src/pages/Layout.tsx
@@ -1,11 +1,16 @@
+
+
 import { Outlet } from "react-router-dom"
+import Sidebar from "../components/Sidebar"
 
 
 const Layout = () => {
   return (
-    <div>
-        Layout
+    <div className="layout-container">
+        <Sidebar />
+        <div className="flex-1 overflow-y-scroll">
         <Outlet />
+        </div>
     </div>
   )
 }
