dec67b4b59aad4abbf68667c673bbcda69f516bf
Priya Satpathy <priyasatpathy429@gmail.com>
2026-05-27 23:56:55 +0530

    content added

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -1,6 +1,7 @@
 import {  ActivityIcon, HomeIcon,  PersonStandingIcon,  UserIcon,  UtensilsIcon } from 'lucide-react'
 import React from 'react'
 import { useTheme } from '../context/ThemeContext'
+import { NavLink } from 'react-router-dom'
 
 const Sidebar = () => {
 
@@ -20,6 +21,19 @@ const Sidebar = () => {
           <PersonStandingIcon className='size-7 text-white'/>
         </div>
     <h1 className='text-2xl font-bold text-slate-800 dark:text-white'>FitTrack</h1>
+    </div>
+
+    <div className='flex flex-col gap-2'>
+      {navItems.map((item)=>(
+        <NavLink key={item.path} to={item.path}
+        className={({ isActive})=> `flex items-center gap-3 px-4 py-2.5
+        border-1-3 transition-all duration-200 ${isActive ? 'bg-emerald-50
+          dark:bg-emerald-900/10 text-emerald-600  dark:text-emerald-400 font-medium':'text-slate-500 dark:text-slate-400 hover:bg-slate-50
+          dark:hover:bg-slate-800 hover:text-slate-700 dark:hover:text-slate-200 border-transparent'}`}>
+          <item.icon className='size-5'/>
+          <span className='text-base'>{item.label}</span>
+        </NavLink>
+      ))}
     </div>
     </nav>
   )
